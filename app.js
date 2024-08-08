11e4674c82e9cfd17f0df91d94b0d039e74a83d3
unknown <rishassimon@gmail.com>
2024-08-08 17:33:00 +0530

    database connection

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
 dotenv.config();
+const connectMongodb = require("./init/mongodb");
 
 //init app
 const app = express();
+
+//connect database
+connectMongodb();
 
 //third-party middleware
 app.use(express.json({ limit: "500mb" }));
